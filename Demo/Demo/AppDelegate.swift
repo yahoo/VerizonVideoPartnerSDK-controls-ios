cfbb4dff686057d8df85785693d312992aed8550
Andrey Moskvin <berkold90@gmail.com>
2017-09-20 11:44:54 +0300

    Reverted changes which were made to support iPhone X

--- Demo/Demo/AppDelegate.swift
+++ Demo/Demo/AppDelegate.swift
@@ -30,8 +30,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let vc = DefaultControlsViewController()
-        vc.view.backgroundColor = .black
-        vc.view.tintColor = .white
+        vc.view.backgroundColor = .green
+        vc.view.tintColor = .blue
         vc.props = props()
         
         window = UIWindow(frame: UIScreen.main.bounds)
