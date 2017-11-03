43da9af14c6e12c9378281dfcb3342d9b9c06bdf
Balavor <pot9ka@gmail.com>
2017-11-03 14:29:55 +0200

    Added SnapshoTest feature

---
+++ Demo/SnapshotTest/HostViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HostViewController.swift
+//  SnapshotTest
+//
+//  Created by rtysiachnik on 11/3/17.
+//  Copyright © 2017 One by AOL : Publishers. All rights reserved.
+//
+
+import Foundation
