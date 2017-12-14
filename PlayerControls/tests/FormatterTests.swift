51d26324e47fcca41d15deed934f67aac5d8db84
Andrey Moskvin <andrey.moskvin@oath.com>
2017-12-14 16:49:14 +0200

    Test fixes

--- PlayerControls/tests/FormatterTests.swift
+++ PlayerControls/tests/FormatterTests.swift
@@ -14,7 +14,7 @@ class FormatterTests: XCTestCase {
         
         //"returns 'h:mm:ss' format"
         XCTAssertEqual(TimeFormatter.string(from: 3600), "1:00:00")
-        XCTAssertEqual(TimeFormatter.string(from: UInt(3600 + 2 * 60 + 2)), "1:02:02")
+        XCTAssertEqual(TimeFormatter.string(from: Int(3600 + 2 * 60 + 2)), "1:02:02")
 
         //"returns 'hh:mm:ss' format"
         XCTAssertEqual(TimeFormatter.string(from: 3600 * 10), "10:00:00")
