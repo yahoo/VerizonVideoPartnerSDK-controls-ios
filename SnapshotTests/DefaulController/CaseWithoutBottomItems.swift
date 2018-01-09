fa4c1537fbe3cb8806b5c642dae03ca166b9a31f
RomanTysiachnik <pot9ka@gmail.com>
2018-01-09 15:16:24 +0200

    Added test case with full progress, replay button and without bottom items

---
+++ SnapshotTests/DefaulController/CaseWithoutBottomItems.swift
@@ -0,0 +1,50 @@
+// Copyright © 2017 Oath. All rights reserved.
+
+import SnappyShrimp
+@testable import PlayerControls
+
+class CaseWithoutBottomItems: SnapshotTest  {
+    
+    var controller: DefaultControlsViewController {
+        let controller = DefaultControlsViewController()
+        controller.view.backgroundColor = .red
+        controller.view.tintColor = .blue
+        
+        controller.props = DefaultControlsViewController.Props.player(
+            DefaultControlsViewController.Props.Player { player in
+                player.playlist = nil
+                player.item = DefaultControlsViewController.Props.Item.playable(
+                    ContentControlsViewController.Props.Controls { controls in
+                        
+                        controls.seekbar = .init()
+                        controls.seekbar?.duration = 3600
+                        controls.seekbar?.currentTime = 3600
+                        controls.seekbar?.progress = 1
+                        controls.seekbar?.buffered = 1
+                        
+                        controls.live.isHidden = true
+                        controls.playbackAction = .replay(.nop)
+                        
+                        controls.camera = nil
+                        
+                        controls.settings = .hidden
+                        controls.pictureInPictureControl = .unsupported
+                        controls.airplay = .hidden
+                        
+                        controls.legible = .`internal`(nil)
+                })
+        })
+        
+        return controller
+    }
+    
+    func test() {
+        if #available(iOS 10.0, *) {
+            verify(controller, for: Device.iPadPro9.portrait.fullScreen)
+        }
+        if #available(iOS 11.0, *) {
+            verify(controller, for: Device.iPhoneX.portrait)
+            verify(controller, for: Device.iPhoneX.landscapeRight)
+        }
+    }
+}
