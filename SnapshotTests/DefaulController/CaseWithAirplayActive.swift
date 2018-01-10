ea4c49827c281d27171ed01b803fdd3728f37a4f
RomanTysiachnik <pot9ka@gmail.com>
2018-01-10 16:35:39 +0200

    Added test case with an active AirPlay

---
+++ SnapshotTests/DefaulController/CaseWithAirplayActive.swift
@@ -0,0 +1,86 @@
+// Copyright © 2017 Oath. All rights reserved.
+
+import SnappyShrimp
+@testable import PlayerControls
+
+class CaseWithAirplayActive: SnapshotTest {
+    
+    var controller: DefaultControlsViewController {
+        let controller = DefaultControlsViewController()
+        controller.view.backgroundColor = .red
+        controller.view.tintColor = .blue
+        
+        controller.props = DefaultControlsViewController.Props.player(
+            DefaultControlsViewController.Props.Player { player in
+                player.playlist = DefaultControlsViewController.Props.Playlist()
+                player.playlist?.next = .nop
+                player.playlist?.prev = .nop
+                player.item = DefaultControlsViewController.Props.Item.playable (
+                    DefaultControlsViewController.Props.Controls { controls in
+                        
+                        controls.title = "Not very long title"
+                        controls.seekbar = .init()
+                        controls.seekbar?.duration = 360
+                        controls.seekbar?.currentTime = 180
+                        controls.seekbar?.progress = 0.5
+                        controls.seekbar?.buffered = 0.5
+                        
+                        controls.live.isHidden = true
+                        controls.playbackAction = .play(.nop)
+                        
+                        controls.sideBarViewHidden = false
+                        
+                        controls.camera = DefaultControlsViewController.Props.Camera()
+                        
+                        controls.settings = .disabled
+                        controls.pictureInPictureControl = .impossible
+                        
+                        controls.airplay = .active
+                        
+                        controls.legible = .`internal`(nil)
+                })
+        })
+        controller.sidebarProps = sideProps()
+        return controller
+    }
+    
+    func test() {
+        if #available(iOS 11.0, *) {
+            verify(controller, for: Device.iPhoneX.landscapeRight)
+        }
+        if #available(iOS 11.0, *) {
+            verify(controller, for: Device.iPadPro9.portrait.oneThird)
+            verify(controller, for: Device.iPadPro10.portrait.oneThird)
+            
+            verify(controller, for: Device.iPadPro10.landscape.oneThird)
+        }
+    }
+    
+    func sideProps() -> [SideBarView.ButtonProps]{
+        
+        let shareIcons = SideBarView.ButtonProps.Icons(
+            normal: UIImage(named: "icon-share", in: Bundle(for: SideBarView.self), compatibleWith: nil)!,
+            selected: nil,
+            highlighted: UIImage(named: "icon-share-active", in: Bundle(for: SideBarView.self), compatibleWith: nil))
+        
+        let share = SideBarView.ButtonProps(
+            isEnabled: true,
+            isSelected: false,
+            icons: shareIcons,
+            handler: .nop)
+        
+        let favoriteIcon = SideBarView.ButtonProps.Icons(
+            normal: UIImage(named: "icon-fav", in: Bundle(for: SideBarView.self), compatibleWith: nil)!,
+            selected: nil,
+            highlighted: UIImage(named: "icon-fav-active", in: Bundle(for: SideBarView.self), compatibleWith: nil))
+        
+        let favorite = SideBarView.ButtonProps(
+            isEnabled: true,
+            isSelected: false,
+            icons: favoriteIcon,
+            handler: .nop)
+        
+        return [favorite, share]
+    }
+}
+
