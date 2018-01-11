f39ce46888d20b84eae4f52eb17df1772a584026
RomanTysiachnik <pot9ka@gmail.com>
2018-01-11 16:47:35 +0200

    Added test cases for Ad Video controller

---
+++ SnapshotTests/AdVideoController/CaseWithAirPlayActive.swift
@@ -0,0 +1,32 @@
+// Copyright © 2017 Oath. All rights reserved.
+
+
+import SnappyShrimp
+@testable import PlayerControls
+
+class CaseWithAirPlayActive: SnapshotTest {
+    
+    var controller: AdVideoControls {
+        let controller = AdVideoControls()
+        controller.view.backgroundColor = .red
+        controller.view.tintColor = .blue
+        
+        controller.props = AdVideoControls.Props(
+            mainAction: AdVideoControls.Props.MainAction.play(.nop),
+            seeker: AdVideoControls.Props.Seeker(remainingPlayTime: "0:30", currentValue: 0.5),
+            tapAction: .nop,
+            isLoading: false, airplayActiveViewHidden: false)
+        return controller
+    }
+    
+    func test() {
+        if #available(iOS 11.0, *) {
+            verify(controller, for: Device.iPhoneX.portrait)
+            verify(controller, for: Device.iPhoneX.landscapeLeft)
+        }
+        if #available(iOS 11.0, *) {
+            verify(controller, for: Device.iPadPro9.portrait.oneThird)
+        }
+    }
+}
+
