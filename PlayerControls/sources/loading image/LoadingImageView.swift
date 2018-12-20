0ff8b4912bab681ceea267ec238c5ebae2d16af5
Vladyslav Anokhin <vladyslav.anokhin@oath.com>
2018-12-20 15:46:45 +0200

    Update enable rotation method

--- PlayerControls/sources/loading image/LoadingImageView.swift
+++ PlayerControls/sources/loading image/LoadingImageView.swift
@@ -16,15 +16,13 @@ public class LoadingImageView: UIImageView {
     }
     
     func enableRotation() {
-        /* Configure loading animation. */ do {
-            guard layer.animation(forKey: "Spin") == nil else { return }
-            let rotation = CABasicAnimation(keyPath: "transform.rotation")
-            rotation.fromValue = 0
-            rotation.toValue = 2 * Double.pi
-            rotation.duration = 1.1
-            rotation.repeatCount = .greatestFiniteMagnitude
-            layer.add(rotation, forKey: "Spin")
-        }
+        guard layer.animation(forKey: "Spin") == nil else { return }
+        let rotation = CABasicAnimation(keyPath: "transform.rotation")
+        rotation.fromValue = 0
+        rotation.toValue = 2 * Double.pi
+        rotation.duration = 1.1
+        rotation.repeatCount = .greatestFiniteMagnitude
+        layer.add(rotation, forKey: "Spin")
     }
     
     func disableRotation() {
