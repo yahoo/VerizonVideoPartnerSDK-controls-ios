bbeaf9b9e1b448e01d650f4477908e5fe0951cd1
Yi-Chin Sun <yichin.sun@verizonmedia.com>
2020-12-04 13:19:46 -0500

    Fix for Travis

---
+++ PlayerControls/PlayerControlsHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerControlsHost
+//
+//  Created by Yi-Chin Sun on 12/4/20.
+//  Copyright © 2020 Oath Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
